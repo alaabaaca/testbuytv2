5589b6aa512253464be9d112890bcf96970b5a3f
thinh_hung <thinh_hung@thinhhung.(none)>
2013-04-16 20:22:11 +0700

    f

--- config/application.rb
+++ config/application.rb
@@ -64,10 +64,6 @@ module BuytCanTho
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-
-    #disable css
-    config.generators do |g|
-      g.stylesheets false
-    end
+    config.assets.initialize_on_precompile = false
   end
 end
