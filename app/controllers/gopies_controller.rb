5d88b5b5e07d1c08c238ec50d06f95ba21cb3d9c
alaabaaca <alaabaaca@gmail.com>
2013-05-04 11:08:37 +0700

    f

--- app/controllers/gopies_controller.rb
+++ app/controllers/gopies_controller.rb
@@ -46,14 +46,12 @@ class GopiesController < ApplicationController
   def gopies_android
     puts "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxparams[:gopy]xx"
     puts params[:gopy]
-    render :json => {"status" => 2}
-    
+   
     @gopy = Gopy.new(params[:gopy])
     
     if @gopy.save
       render :json => {"status" => 200}
-   
-      
+
     end
   end
 
