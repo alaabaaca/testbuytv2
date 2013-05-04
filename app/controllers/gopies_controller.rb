42cbfcb6f58c4057c03234484e18d3d5da1ab27a
alaabaaca <alaabaaca@gmail.com>
2013-05-04 11:53:18 +0700

    f

--- app/controllers/gopies_controller.rb
+++ app/controllers/gopies_controller.rb
@@ -1,16 +1,15 @@
 class GopiesController < ApplicationController
   authorize_resource
   skip_authorize_resource :only => [:gopies_android]
-
   # GET /gopies
   # GET /gopies.json
-  def index
+  def index_origin
     require 'will_paginate/array'
 
     session[:taikhoan] = current_user.email
     @t = session[:taikhoan]
 
-      @taikhoan = User.find_by_email(@t)
+    @taikhoan = User.find_by_email(@t)
     if (@taikhoan != nil) then
       @congty = Congty.find_by_id(@taikhoan.mact)
 
@@ -27,15 +26,43 @@ class GopiesController < ApplicationController
           end
         end
       end
-       @gopies = @gop.paginate(:page => params[:page], :per_page => 10)
+      @gopies = @gop.paginate(:page => params[:page], :per_page => 10)
     end
-
-
-
-
 
     #@gopies = Gopy.find_all_by_congty(@congty.id)
 
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @gopies }
+    end
+  end
+
+  def index
+    require 'will_paginate/array'
+
+    session[:taikhoan] = current_user.email
+    @t = session[:taikhoan]
+
+    @taikhoan = User.find_by_email(@t)
+    if (@taikhoan != nil) then
+      @congty = Congty.find_by_id(@taikhoan.mact)
+
+      @chuyens = Chuyen.order('biensoxe ASC').find_all_by_mact(@congty.id)
+      @gopy = Gopy.order('ngay ASC').all
+
+      #@gopies = Array.new
+      @gop =  []
+
+      @gopy.each do |g|
+        @chuyens.each do |bs|
+          if (g.biensoxe == bs.biensoxe) then
+          #@gopies.append(@gopy)
+          @gop << g
+          end
+        end
+      end
+      @gopies = @gop.paginate(:page => params[:page], :per_page => 10)
+    end
 
     respond_to do |format|
       format.html # index.html.erb
@@ -46,9 +73,9 @@ class GopiesController < ApplicationController
   def gopies_android
     puts "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxparams[:gopy]xx"
     puts params[:gopy]
-   
+
     @gopy = Gopy.new(params[:gopy])
-    
+
     if @gopy.save
       render :json => {"status" => 200}
 
@@ -86,7 +113,6 @@ class GopiesController < ApplicationController
   # POST /gopies.json
   def create
     @gopy = Gopy.new(params[:gopy])
-
 
     respond_to do |format|
       if @gopy.save
